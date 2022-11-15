5d214f685fedf30fc5450abddcbc9be7c91a13d0
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-14 16:23:32 -0800

    added functions into detail js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
+// on load
 window.addEventListener('load', async () => {
+    // fetch all dogs
     const dogs = await getDogs();
 
     for (let dog of dogs) {
         const dogEl = renderDogCard(dog);
+        // render and append all dog cards to the container
         dogListContainer.append(dogEl);
     }
 });
-// on load
-// fetch all dogs
-// render and append all dog cards to the container
